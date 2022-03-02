b2052123b00f70bcdbc3464125051f66ca8f27f7
Badboy <77886836+aldrinsai6@users.noreply.github.com>
2022-03-02 20:33:19 +0530

    inlinefuncs.py

--- EmikoRobot/utils/inlinefuncs.py
+++ EmikoRobot/utils/inlinefuncs.py
@@ -115,21 +115,21 @@ async def alive_function(answers):
     bot_state = "Dead" if not await app.get_me() else "Alive"
     ubot_state = "Dead" if not await ubot2.get_me() else "Alive"
     buttons.add(
-        InlineKeyboardButton("Main bot", url="https://t.me/EmiexRobot"),
+        InlineKeyboardButton("Main bot", url="https://t.me/Miss_esther_bot"),
         InlineKeyboardButton(
             "Go Inline!", switch_inline_query_current_chat=""
         ),
     )
 
     msg = f"""
-**[Emiko Robot ❤️](https://t.me/emikosupport):**
+**[Esther Robot ❤️](https://t.me/dC_bots_support):**
 **MainBot:** `{bot_state}`
 **UserBot:** `{ubot_state}`
 **Python:** `{pyver.split()[0]}`
 **Pyrogram:** `{pyrover}`
 **MongoDB:** `{mongover}`
 **Platform:** `{sys.platform}`
-**Profiles:** [BOT](t.me/{BOT_USERNAME}) | [UBOT](t.me/excrybaby)
+**Profiles:** [BOT](t.me/{BOT_USERNAME}) | [UBOT](t.me/bRoThEr_Of_ViLlAiN)
 """
     answers.append(
         InlineQueryResultArticle(
